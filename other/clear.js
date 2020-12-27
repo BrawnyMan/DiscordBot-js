0dd69f1f4d4553b5ff79f347ab1240fccfa505d2
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2020-12-27 15:39:06 +0100

    Update clear.js

--- other/clear.js
+++ other/clear.js
@@ -1,5 +1,6 @@
 // Clears messages
 const { prefix } = require("../config.json");
+const { curr_time } = require("../util/curr_time.js");
 module.exports = (message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const amount = parseInt(args[1]) + 1;
@@ -8,7 +9,9 @@ module.exports = (message) => {
   else if (amount <= 1 || amount > 100)
     return message.reply("you need to input a number between 1 and 99.");
   message.channel.bulkDelete(amount, true).catch((err) => {
-    console.error(err);
+    message.client.users.cache
+      .get("345972187007680533")
+      .send(`[${curr_time()}] ${message.author} (${message.content}) ${err}`);
     message.channel.send(
       "there was an error trying to prune messages in this channel!"
     );
