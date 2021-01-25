442b091d3ae91f0f7b8032f30c16bbe3568a962f
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-25 07:25:03 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,10 +5,12 @@ module.exports = {
   description: "Displays your or someone's avatar",
   aliases: ["icon", "pfp"],
   execute(message) {
+    // If user is mentioned
     if (!message.mentions.users.size)
       return message.channel.send(
         message.author.displayAvatarURL({ dynamic: true })
       );
+    // If no user is mentioned, display authors avatar
     return message.channel.send(
       getUserFromMention(message).displayAvatarURL({ dymanic: true })
     );
