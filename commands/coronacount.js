94aa0284c5b7ec955a272960cc8d2899348f5fbf
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-25 07:25:30 +0100

    Update coronacount.js

--- commands/coronacount.js
+++ commands/coronacount.js
@@ -8,32 +8,26 @@ module.exports = {
   usage: "coronacount",
   description: "Corona count",
   aliases: ["cc"],
-  cooldown: 5,
+  cooldown: 60,
   execute(message) {
+    // Get pure html form website/url
     rp(url)
       .then(function (html) {
-        //success
-        let count = $(".maincounter-number > span", html)
-          .text()
-          .substring(0, 10)
-          .split(",")
-          .join("");
-        parseInt(count, 10);
-        let c = $(".maincounter-number > span", html)
-          .text()
-          .substring(0, 10)
-          .split(",")
-          .join(" ");
-        let d = $(".maincounter-number > span", html)
-          .text()
-          .substring(10, 20)
-          .split(",")
-          .join(" ");
-        let r = $(".maincounter-number > span", html)
-          .text()
-          .substring(20, 30)
-          .split(",")
-          .join(" ");
+        // Search for specific part of that website with cheerio
+        let smt = $(".maincounter-number > span", html).text().replace(" ", "");
+        // Get numbers for specific group
+        //-------------------------------
+        // 99,481,692 2,133,39171,513,840
+        //   0   1     2   3     4   5
+        //       !         !
+        //|       ---|      ---|
+        //-------------------------------
+        let commas = [];
+        for (let i = 0; i < smt.length; i++) if (smt[i] == ",") commas.push(i);
+        const CASES = smt.slice(0, commas[1] + 4);
+        const DEATHS = smt.slice(commas[1] + 4, commas[3] + 4);
+        const RECOVERED = smt.slice(commas[3] + 4, smt.length);
+        // Make embed
         const embed = new MessageEmbed()
           .setColor("#FF0000")
           .setTitle("**__Corona Count__**")
@@ -41,16 +35,18 @@ module.exports = {
             "https://www.worldometers.info/img/worldometers-logo.gif"
           )
           .addFields(
-            { name: "**Cases: **", value: `${c}` },
-            { name: "**Deaths: **", value: `${d}` },
-            { name: "**Recovered: **", value: `${r}` }
+            { name: "**Cases: **", value: `${CASES.replace(/,/g, " ")}` },
+            { name: "**Deaths: **", value: `${DEATHS.replace(/,/g, " ")}` },
+            {
+              name: "**Recovered: **",
+              value: `${RECOVERED.replace(/,/g, " ")}`,
+            }
           );
-        message.channel.bulkDelete(1);
         message.channel.send(embed);
       })
       .catch(function (err) {
         message.client.users.cache
-          .get("<your_ID>")
+          .get("<your_id>")
           .send(
             `[${curr_time()}] ${message.author} (${message.content}) ${err}`
           );
