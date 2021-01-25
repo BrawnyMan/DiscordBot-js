cfd2c6b797141c66808438c5d25cf30318b9a14e
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-25 07:25:51 +0100

    Update dc.js

--- commands/dc.js
+++ commands/dc.js
@@ -5,6 +5,7 @@ module.exports = {
   aliases: ["leave"],
   execute(message) {
     const voiceChannel = message.member.voice.channel;
+    // If channel does not exists, can not leave it
     if (!voiceChannel)
       return message.channel.send(
         "You need to be in a voice channel to disconnect a bot!"
