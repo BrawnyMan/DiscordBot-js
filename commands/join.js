e9861d8d145e9baebea22ee534d213d396e369e4
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-27 09:08:01 +0100

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -2,15 +2,14 @@ module.exports = {
   name: "join",
   usage: "join",
   description: "Joins the voice channel",
-  cooldown: 5,
   async execute(message) {
     const voiceChannel = message.member.voice.channel;
+    // No channel, no join
     if (!voiceChannel)
       return message.reply(" You need to be in a voice channel to play music!");
     try {
       await voiceChannel.join();
     } catch (err) {
-      console.log(err);
       return message.channel.send(err);
     }
   },
