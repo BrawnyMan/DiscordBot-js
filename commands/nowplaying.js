7ad6d618a5114380c0b8530ff0fc397fc243047e
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-28 14:48:33 +0100

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -3,22 +3,16 @@ module.exports = {
   name: "nowplaying",
   usage: "nowplaying",
   description: "Get the song that is playing",
-  cooldown: 5,
   aliases: ["np"],
   execute(message) {
+    // Getting queue and displaying currently playing voice
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue) return message.channel.send("There is nothing playing.");
     const embed = new MessageEmbed().setAuthor(
       "Now playing 🎵",
       `${message.client.user.displayAvatarURL()}`
     );
-    if (serverQueue.songs[0].yt) {
-      embed
-        .setTitle(`${serverQueue.songs[0].title}`)
-        .setURL(`${serverQueue.songs[0].url}`);
-    } else {
-      embed.setTitle(`${serverQueue.songs[0].title} (List)`);
-    }
+    embed.setTitle(`${serverQueue.songs[0].title}`);
     return message.channel.send(embed);
   },
 };
