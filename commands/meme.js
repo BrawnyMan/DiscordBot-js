73349ecbc807ff6239763f39e10b510272464acf
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-28 14:47:52 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -3,6 +3,7 @@ module.exports = {
   name: "meme",
   usage: "meme",
   description: "Posts a meme",
+  // Get url of image or .gif for random meme
   async execute(message) {
     const { image } = await fetch(
       "https://some-random-api.ml/meme"
