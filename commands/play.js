f1391de3d69ae48c386fca268dfc1277cc0d7d35
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2021-01-30 13:49:28 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -6,27 +6,30 @@ const fs = require("fs");
 module.exports = {
   name: "play",
   usage: "play < nameOfVoice >",
-  description: "Sing a song in your voice channel",
+  description: "Plays a voice from music folder",
   args: true,
   cooldown: 5,
-  async execute(message) {
+  async execute(message, loop = 0) {
     try {
+      // Getting name of the voice
       const args = message.content.split(/ +/);
       args.shift();
       const queue = message.client.queue;
       const serverQueue = queue.get(message.guild.id);
       const voiceChannel = message.member.voice.channel;
+      // If not in channel, join the one where the author (of this message) is
       if (!voiceChannel)
         return message.reply(
           " You need to be in a voice channel to play music!"
         );
       let song = {
         title: args[0],
-        url: "./music_file/" + args[0] + ".mp3",
+        path: "./music_folder/" + args[0] + ".mp3",
         /*yt: false, len: 0,*/
       };
-      if (!fs.existsSync(song.url))
+      if (!fs.existsSync(song.path))
         return message.channel.send("This song does not exists!");
+      // Create queue
       if (!serverQueue) {
         const queueContruct = {
           textChannel: message.channel,
@@ -34,12 +37,13 @@ module.exports = {
           connection: null,
           songs: [],
           volume: 0.5,
-          loop: 0,
+          loop: loop,
           playing: true,
         };
         queue.set(message.guild.id, queueContruct);
         queueContruct.songs.push(song);
         try {
+          // Try to play voice
           let connection = await voiceChannel.join();
           queueContruct.connection = connection;
           //queueContruct.textChannel.send(`🎶 Playing: **${song.title}**`);
@@ -54,7 +58,6 @@ module.exports = {
             "Cannot add more than 100 songs to queue!"
           );
         serverQueue.songs.push(song);
-        //const ifList = song.yt ? `${song.title}` : `${song.title}`; There should be YT music title if YT is selected
         const embed = new MessageEmbed()
           .setAuthor(
             "Added to queue",
@@ -81,16 +84,16 @@ module.exports = {
     /*if (song.yt) // for YouTube
             dispatcher = serverQueue.connection.play(ytdl(song.url, { filter:"audioonly" }));
         else*/
-    dispatcher = serverQueue.connection.play(song.url);
+    dispatcher = serverQueue.connection.play(song.path);
     dispatcher
       .on("finish", () => {
-        // if loop no shift
+        // Loop or not
         if (serverQueue.loop == 0) serverQueue.songs.shift();
         this.play(message, serverQueue.songs[0]);
       })
       .on("error", (error) =>
         message.client.users.cache
-          .get("<your_ID>")
+          .get("<your_id>")
           .send(
             `[${curr_time()}] ${message.author} (${message.content}) ${error}`
           )
